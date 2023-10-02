41df705c64e6843ef9ed2e9a6c9f7deafc3235ce
SalsabilaYulianti <141704344+SalsabilaYulianti@users.noreply.github.com>
2023-10-02 13:13:15 +0700

    Add files via upload

---
+++ profile_arrow_function.js
@@ -0,0 +1,6 @@
+const login = (namaDepan, namaBelakang, email ) => {
+  console.log("Login Screen " + namaDepan + " " + namaBelakang)
+  console.log("email " + email);
+}
+
+login("Salsabila", "yulianti", "222310279@smkn4-tsm.sch.id")
